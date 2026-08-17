c92b7a1b5fd5313e0b49909365d5cd88f58a25a4
GioMjds <giomjds@gmail.com>
2026-08-17 18:58:19 +0800

    Add NamedPipeServerFactory for secure, cross-platform IPC
    Introduced PrintBit.Infrastructure.IPC.NamedPipeServerFactory with a Create method to instantiate NamedPipeServerStream. On Windows, applies custom PipeSecurity for user and group access via NamedPipeServerStreamAcl.Create; on other platforms, uses the default constructor. Enhances named pipe server security and cross-platform compatibility.

---
+++ src/PrintBit.Infrastructure/IPC/NamedPipeServerFactory.cs
@@ -0,0 +1,57 @@
+using System.IO.Pipes;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace PrintBit.Infrastructure.IPC;
+
+public static class NamedPipeServerFactory
+{
+    public static NamedPipeServerStream Create(
+        string pipeName,
+        PipeDirection direction,
+        int maxNumberOfServerInstances = NamedPipeServerStream.MaxAllowedServerInstances,
+        PipeTransmissionMode transmissionMode = PipeTransmissionMode.Byte,
+        PipeOptions options = PipeOptions.Asynchronous)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            var pipeSecurity = new PipeSecurity();
+
+            using var currentIdentity = WindowsIdentity.GetCurrent();
+            if (currentIdentity.User != null)
+            {
+                pipeSecurity.AddAccessRule(new PipeAccessRule(
+                    currentIdentity.User,
+                    PipeAccessRights.FullControl,
+                    AccessControlType.Allow));
+            }
+
+            pipeSecurity.AddAccessRule(new PipeAccessRule(
+                new SecurityIdentifier(WellKnownSidType.AuthenticatedUserSid, null),
+                PipeAccessRights.ReadWrite | PipeAccessRights.CreateNewInstance,
+                AccessControlType.Allow));
+
+            pipeSecurity.AddAccessRule(new PipeAccessRule(
+                new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                PipeAccessRights.ReadWrite,
+                AccessControlType.Allow));
+
+            return NamedPipeServerStreamAcl.Create(
+                pipeName,
+                direction,
+                maxNumberOfServerInstances,
+                transmissionMode,
+                options,
+                inBufferSize: 0,
+                outBufferSize: 0,
+                pipeSecurity);
+        }
+
+        return new NamedPipeServerStream(
+            pipeName,
+            direction,
+            maxNumberOfServerInstances,
+            transmissionMode,
+            options);
+    }
+}
